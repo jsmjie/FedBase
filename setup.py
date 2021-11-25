5decd9cb08aff3c2288a6afaed764dbed41cfffc
jie-ma-ai <jsmjie@hotmail.com>
2021-11-25 20:56:22 +1100

    upload pypi test 0.1.0

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fedbase",
+    version="0.1.0",
+    author="Jie MA",
+    author_email="ustcmj@gmail.com, jie.ma-5@student.uts.edu.au",
+    description="An easy, accessible Federated Learning framework for individual researchers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jie-ma-ai/FedBase",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
