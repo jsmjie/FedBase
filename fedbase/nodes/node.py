fix bug of increasing memory due to gradients

fedbase/nodes/node.py
@@ -73,12 +73,13 @@ class node():
         # optim
         self.loss = self.objective(outputs, labels)
         self.loss.backward()
+
         # calculate accumulate gradients
-        grads = torch.tensor([])
-        for index, param in enumerate(self.model.parameters()):
-            # param.grad = torch.tensor(grads[index])
-            grads= torch.cat((grads, torch.flatten(param.grad).cpu()),0)
-        self.grads.append(grads)
+        # grads = torch.tensor([])
+        # for index, param in enumerate(self.model.parameters()):
+        #     # param.grad = torch.tensor(grads[index])
+        #     grads= torch.cat((grads, torch.flatten(param.grad).cpu()),0)
+        # self.grads.append(grads)
         
         self.optim.step()
         # self.train_steps+=1